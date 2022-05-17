adb6cb27947cb4a4af42afe7b5e934d2590420e2
Max <76849308+Max0824@users.noreply.github.com>
2022-05-16 20:25:12 -0500

    Initial Commit (Files Only)

---
+++ MenuFunctions.py
@@ -0,0 +1,36 @@
+from tkinter import *
+def Script2App():
+    pass
+def IPLocator():
+    pass
+def EditZeroService():
+    pass
+def ServerIPStatusCheck():
+    pass
+def EnableOfflineMode():
+    pass
+def BrowseZeroService():
+    pass
+def QuickTools():
+    pass
+def AnonymousEmail():
+    pass
+def CloudZeroService():
+    pass
+def CMPTRAnalyze():
+    pass
+def SethcHack():
+    pass
+def OSDownload():
+    pass
+def BootableMediaCreate():
+    pass
+def CompareFilePrpts():
+    pass
+def About():
+    aboutWin = Tk()
+    aboutWin.geometry("450x450")
+    aboutWin.title("About")
+    logo = Label(aboutWin, image=PhotoImage("favicon.png"))
+    logo.place(x=0, y=0)
+    aboutWin.mainloop()
