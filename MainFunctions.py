ddc52b411589d2a0dee416922e8305f3adb9c750
The-HackZero-Team <maxwellp.584@gmail.com>
2022-05-19 19:38:58 -0500

    Update Files

---
+++ MainFunctions.py
@@ -0,0 +1,8 @@
+def EnableOfflineMode():
+    pass
+def EditZeroService():
+    pass
+def CloudZeroService():
+    pass
+def BrowseZeroService():
+    pass
---
+++ MenuFunctions.py
@@ -0,0 +1,28 @@
+from tkinter import *
+def Script2App():
+    pass
+def IPLocator():
+    pass
+def ServerIPStatusCheck():
+    pass
+def QuickTools():
+    pass
+def AnonymousEmail():
+    pass
+def CMPTRAnalyze():
+    pass
+def SethcHack():
+    pass
+def OSDownload():
+    pass
+def BootableMediaCreate():
+    pass
+def CompareFilePrpts():
+    pass
+def About():
+    aboutWin = Tk()
+    aboutWin.geometry("450x450")
+    aboutWin.title("About")
+    logo = Label(aboutWin, image=PhotoImage("favicon.png"))
+    logo.place(x=0, y=0)
+    aboutWin.mainloop()
